5d665b1b37f28a4c5895f999fa93dd6cfadcc2e3
Egor Eremeev <nord@dezigner.ru>
2014-06-25 12:45:08 +0400

    Переход на POST и плюс редактирования и удаление в одном окне

--- routes/addObject.js
+++ routes/addObject.js
@@ -5,12 +5,7 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
-/* GET home page. */
-router.get('/', function(req, res) {
-    res.render('addObject', { reqParam: req.param("Naim") });
-});
-
-router.get('/add', function(req, res) {
+router.post('/', function(req, res) {
     var outputFilename = 'db.json';
     var jsonString = JSON.parse(fs.readFileSync(outputFilename, 'utf8'));
 
@@ -21,17 +16,17 @@ router.get('/add', function(req, res) {
     var newId = parseInt(id) + 1
 
     jsonObj.id = newId;
-    jsonObj.at_syst = req.param("at_syst");
-    jsonObj.dom = req.param("dom");
-    jsonObj.Den = req.param("Den");
-    jsonObj.Naim = req.param("Naim");
-    jsonObj.At_Type = req.param("At_Type");
-    jsonObj.At_Len = req.param("At_Len");
-    jsonObj.At_Dec = req.param("At_Dec");
-    jsonObj.At_Min = req.param("At_Min");
-    jsonObj.At_Max = req.param("At_Max");
-    jsonObj.At_Razm = req.param("At_Razm");
-    jsonObj.At_Kl = req.param("At_Kl");
+    jsonObj.at_syst = req.body.at_syst;
+    jsonObj.dom = req.body.dom;
+    jsonObj.Den = req.body.Den;
+    jsonObj.Naim = req.body.Naim;
+    jsonObj.At_Type = req.body.At_Type;
+    jsonObj.At_Len = req.body.At_Len;
+    jsonObj.At_Dec = req.body.At_Dec;
+    jsonObj.At_Min = req.body.At_Min;
+    jsonObj.At_Max = req.body.At_Max;
+    jsonObj.At_Razm = req.body.At_Razm;
+    jsonObj.At_Kl = req.body.At_Kl;
     jsonString.objects[jsonString.objects.length]=jsonObj;
 
     fs.writeFile(outputFilename, JSON.stringify(jsonString, null, 4), function(err) {});
--- routes/editObject.js
+++ routes/editObject.js
@@ -5,26 +5,14 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
-router.get('/:id', function(req, res) {
-    var jsonString = JSON.parse(fs.readFileSync('db.json', 'utf8'));
-    var selectedObjectID = req.params.id;
-    var selectedObject;
-    for(i=0;i<jsonString.objects.length;i++) {
-        if(jsonString.objects[i].id==selectedObjectID) {
-            selectedObject = jsonString.objects[i];
-            break;
-        }
-    }
-    res.render('editObject', { jsonObject: selectedObject });
-});
 
-router.post('/:id', function(req, res) {
+router.post('/', function(req, res) {
     var jsonString = JSON.parse(fs.readFileSync('db.json', 'utf8'));
-    var selectedObjectID = req.params.id;
+    var selectedObjectID = req.body.id;
     for(i=0;i<jsonString.objects.length;i++) {
         if(jsonString.objects[i].id==selectedObjectID) {
             jsonString.objects[i]={
-                "id": req.params.id,
+                "id": req.body.id,
                 "at_syst": req.body.at_syst,
                 "dom": req.body.dom,
                 "Den": req.body.Den,
